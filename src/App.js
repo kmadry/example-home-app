0bdff128c65e04c2cd988e7ee4cd854ca6fd7e17
mortis133 <kryst.madry@gmail.com>
2023-10-13 18:22:03 +0200

    add mui css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { lazy, Suspense, useState } from 'react'; // Must be imported for webpack to work
+import Button from '@mui/material/Button';
 import './App.css';
+import './MuiClassNameSetup';
+
 
 const Header = lazy(() => import('HeaderApp/Header'));
 const Footer = lazy(() => import('HeaderApp/Footer'));
 
 function App() {
   const [state, setState] = useState('before click');
-  const click = () => setState('afterClick'); 
+  const click = () => setState('afterClick');
+
   return (
     <div className="App">
+      <Button variant="contained">Klik</Button>
       <Suspense fallback={<div>Loading Header...</div>}>
         <Header />
       </Suspense>
