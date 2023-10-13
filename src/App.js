0358feaf7b22d115088e3ede1a1b3b1e10b3736c
mortis133 <kryst.madry@gmail.com>
2023-10-13 14:30:14 +0200

    add mf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { lazy, Suspense, useState } from 'react'; // Must be imported for webpack to work
 import './App.css';
 
+const Header = lazy(() => import('HeaderApp/Header'));
+const Footer = lazy(() => import('HeaderApp/Footer'));
+
 function App() {
+  const [state, setState] = useState('before click');
+  const click = () => setState('afterClick'); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Loading Header...</div>}>
+        <Header />
+      </Suspense>
+      <div className="container">Demo home page</div>
+      <div className="container">State: {state}</div>
+      <Footer date={"2023"} click={click}/>
     </div>
   );
 }
